7be9dea2dc968c6c5329b8a26ae9d6ef512000b1
Dayn Augustson <Daugustson@cnm.edu>
2018-09-08 13:24:06 -0600

    Learned Objects, Interfaces, and API's

---
+++ objects-apis.js
@@ -0,0 +1,32 @@
+/*
+* Objects, Interfaces, and API's
+ */
+
+let dog = {
+	name: "Steve",
+	color: "brown",
+	breed: "Pug",
+	size: "Small",
+	bark: function(typeOfBark) {
+		console.log("Bark!");
+
+	},
+
+};
+
+dog.bark();
+
+function x(y) {
+	y.num = y.num + 5;
+	console.log(y);
+}
+//Passed in by value
+//let y = 5;
+
+//passed in by ref.
+let y = {
+	name: "Tom",
+	num: 10,
+};
+x(y);
+console.log(y);
